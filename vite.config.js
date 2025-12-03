4ac99cf01a5a084264062f29b89ad7d3b2328d65
Nadya Marchenko <nadya.marchenko@idego.io>
2025-12-03 09:06:50 +0100

    gh pages setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // If your repository name on GitHub is different, update the base path accordingly:
+  // e.g. base: '/your-repo-name/'
+  base: '/privacy-blur-fe/',
   plugins: [react()],
 })
